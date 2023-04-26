7983689dd9cb16e2b4222d48dad2e0d57c269fa0
Scott Mysliwy <scottmysliwy@Scotts-MacBook-Pro.local>
2023-04-26 17:15:36 -0400

    changed to use telescope

--- lua/builder/init.lua
+++ lua/builder/init.lua
@@ -1,14 +1,38 @@
 -- builder.lua
 
 -- Read the build.json file
+local telescope = require("telescope.builtin")
 local build_json = vim.fn.json_decode(vim.fn.readfile(".build.json"))
 
+-- Use Telescope to select a command to execute
+local function execute_command(commands)
+  local items = {}
+
+  for _, cmd in ipairs(commands) do
+    table.insert(items, {
+      display = cmd,
+      cmd = cmd
+    })
+  end
+
+  telescope.vim_buffer_list({
+    prompt_title = "Select a command",
+    entries = items,
+    attach_mappings = function(_, map)
+      map('i', '<CR>', function(prompt_bufnr)
+        local selection = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+        vim.cmd("enew | terminal " .. selection.cmd)
+        return true
+      end)
+      return true
+    end
+  })
+end
 
 -- Execute build command
 local function build()
   if not vim.tbl_isempty(build_json.build) then
-    local cmd = table.concat(build_json.build, " ")
-    vim.cmd("terminal " .. cmd)
+    execute_command(build_json.build)
   else
     print("Build command not found in build.json")
   end
@@ -17,8 +41,7 @@ end
 -- Execute run command
 local function run()
   if not vim.tbl_isempty(build_json.run) then
-    local cmd = table.concat(build_json.run, " ")
-    vim.cmd("terminal " .. cmd)
+    execute_command(build_json.run)
   else
     print("Run command not found in build.json")
   end
@@ -27,8 +50,7 @@ end
 -- Execute test command
 local function test()
   if not vim.tbl_isempty(build_json.test) then
-    local cmd = table.concat(build_json.test, " ")
-    vim.cmd("terminal " .. cmd)
+    execute_command(build_json.test)
   else
     print("Test command not found in build.json")
   end
